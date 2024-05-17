5ff4cf769f7e4657db2c8b7a3240113576328fc5
Sikang Sun <sun1017@data.cs.purdue.edu>
2024-05-17 02:41:42 -0400

    KN2 second commit fix bugs

--- tree/node_inline.h
+++ tree/node_inline.h
@@ -157,10 +157,10 @@ inline char* string_conv(const char* key, int &keylen) {//unnormalized to normal
 
 
 inline long word_cmp(Stdhead* header,const char* key, int keylen, Node *cursor) {
-    int cmp = *(int*)key - *(int*)header->prefix;
+    int cmp = *(int*)key - *(int*)header->key_prefix;
     if (cmp == 0 && keylen > PV_SIZE && header->key_len > PV_SIZE) {
         char *suffix = PageOffset(cursor, header->key_offset);
-        for (int idx = 1; idx < min(header->key_len, keylen); idx += 4) {
+        for (int idx = 1; idx < min((int)header->key_len, keylen); idx += 4) {
             cmp = *(int *)(key + idx * PV_SIZE) - *(int*)(suffix + idx * PV_SIZE);
             if (cmp != 0) return cmp;
         }
