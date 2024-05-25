f78369a0f6e2f5e3eb1ffb422acdc3254cb8f73e
Sikang Sun <sun1017@data.cs.purdue.edu>
2024-05-25 18:05:50 -0400

    debug 4834

--- benchmark/benchmark.h
+++ benchmark/benchmark.h
@@ -73,7 +73,7 @@ public:
         for (uint32_t i = 0; i < values.size(); i++)
             if (_tree->search(values.at(i)) == -1) {
                 #ifdef CHECK
-                        if (i == 2809) {
+                        if (i == 4834) {
                     vector<bool> flag(values.size());
                     _tree->printTree(_tree->getRoot(), flag, true);
             }
